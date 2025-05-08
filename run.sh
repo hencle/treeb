7a2928eb075a74f1e62ffb056f403b9173422282
Hendrik Clemens <hendrikclemens@gmail.com>
2025-05-08 17:54:40 +0200

    Make run.sh executable

--- run.sh
+++ run.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
+# ─── self-make-executable & re-exec ───
+if [[ "${BASH_SOURCE[0]}" == "$0" && ! -x "$0" ]]; then
+  chmod +x "$0"
+  exec "$0" "$@"
+fi
+
 set -e
+
 python3 -m venv .venv
 . .venv/bin/activate
 pip install flask
